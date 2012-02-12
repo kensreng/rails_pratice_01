9686f5bef3c00889a32722c4bb65daf558efb3f1
InSTEDD <instedd@instedd-ThinkPad-T410.(none)>
2012-02-12 13:30:54 +0700

    add repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsPractice01::Application.config.secret_token = '90193a73a666fe05dd6a38978ddd9c6254c4ed6dbd43d4a7896a8a8d1e7c6ce644da3d4e3b00ae33864b7e2f64fe16a86ac3b306aae4bea8cd70e07d0d281bfa'
